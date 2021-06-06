5ce97c22377ddc59be77cab653882b59507bc73d
Niklas Furberg <niklas.furberg@dynabyte.se>
2021-06-06 22:11:05 +0200

    WIP: has two consequtive boards in each data row, fixes correct data count problem

--- prep.py
+++ prep.py
@@ -7,7 +7,7 @@ game_count = 0
 start_year = 2020
 stop_year = 2020
 
-#scan data
+#scan data size
 for year in range(start_year, stop_year+1):
     result_dir = 'data\gomocup' + str(year) + 'results_test'
     for rootdir, dirs, files in os.walk(result_dir):
@@ -19,14 +19,14 @@ for year in range(start_year, stop_year+1):
             for filename in os.listdir(in_directory):
                 if not filename.endswith(".psq"): 
                     continue
-#                game_count += 1#TODO: fix this, it crashes with this uncommented
+                game_count += 1
                 with open(in_directory + filename) as file_in:
-                    next(file_in)
+                    next(file_in)#skip header row
                     lines = []
                     for line in file_in:
                         if line.count(',') == 2:
-                            row_count = row_count + 1
-data_count = row_count - (game_count * 7)#minus header row and init rows for each game
+                            row_count += 1
+data_count = row_count - (game_count * 6)#skip init rows for each game
 
 print('game_count = ' + str(game_count))
 print('row_count = ' + str(row_count))
@@ -48,7 +48,6 @@ for year in range(start_year, stop_year+1):
             for filename in os.listdir(in_directory):
                 if not filename.endswith(".psq"): 
                     continue
-                game = numpy.full((400, 400), -1, dtype=int)
                 board = numpy.zeros(shape=(20,20))
                 player = 1
                 eof = False
@@ -59,12 +58,12 @@ for year in range(start_year, stop_year+1):
                     row = 1
                     lines = []
                     for line in file_in:
-                        row += 1
                         if line.count(',') == 2:
                             board[int(line.split(',')[0])-1][int(line.split(',')[1])-1] = player
-                            if row > 7:#only add non init rows as actual data, first six rows are init moves
-                                game[data_offset] = board.ravel().astype(int)
-                                #data[data_index] = board.ravel().astype(int)//TODO: move out of this loop and group every two moves
+                            if row >= 7:#only add non init rows as actual moves, first six rows are init moves
+                                data[data_index,400:] = board.ravel().astype(int)
+                                if row >= 8:
+                                    data[data_index,:400] = data[data_index,400:]
                                 data_index += 1
                                 data_offset += 1
                         elif eof:
@@ -75,43 +74,50 @@ for year in range(start_year, stop_year+1):
                             player = 2
                         else:
                             player = 1
-                #if winner == 1:
-                #    labels[data_index-data_offset:data_index:2] = 1
-                #else:
-                #    labels[data_index+1-data_offset:data_index:2] = 1
+                        row += 1
+                if winner == 1:
+                    labels[data_index-data_offset:data_index:2] = 1
+                else:
+                    labels[data_index+1-data_offset:data_index:2] = 1
             group = group +1
-            print('game = ' + str(game))
 
 print(data.shape)
+print(data)
 
 print(labels.shape)
+print(labels)
 
 #shuffle and store data
-#shuffled_index = numpy.zeros(row_count).astype(int)
-#for i in range(row_count):
-#    shuffled_index[i] = i
+shuffled_index = numpy.zeros(data_count).astype(int)
+for i in range(data_count):
+    shuffled_index[i] = i
 
 #numpy.random.shuffle(shuffled_index)
-#print(shuffled_index)
+print(shuffled_index)
 
 
 #train_count = int(row_count * 0.6)
 #test_count = int(row_count * 0.25)
 #val_count = int(row_count - train_count - test_count)
-#print('train_count = ' + str(train_count))
-#print('test_count = ' + str(test_count))
-#print('val_count = ' + str(val_count))
+train_count = int(data_count)
+test_count = 0
+val_count = 0
+print('train_count = ' + str(train_count))
+print('test_count = ' + str(test_count))
+print('val_count = ' + str(val_count))
 
-#train_data = numpy.zeros(shape=(train_count, 400))
-#train_labels = numpy.zeros(shape=(train_count, 1))
-#test_data = numpy.zeros(shape=(test_count, 400))
-#test_labels = numpy.zeros(shape=(test_count, 1))
-#val_data = numpy.zeros(shape=(val_count, 400))
-#val_labels = numpy.zeros(shape=(val_count, 1))
+train_data = numpy.zeros(shape=(train_count, 400*2))
+train_labels = numpy.zeros(shape=(train_count, 1))
+test_data = numpy.zeros(shape=(test_count, 400*2))
+test_labels = numpy.zeros(shape=(test_count, 1))
+val_data = numpy.zeros(shape=(val_count, 400*2))
+val_labels = numpy.zeros(shape=(val_count, 1))
 
-#for i in range(train_count):
-    #train_data[i] = data[shuffled_index[i]]
-    #train_labels[i] = labels[shuffled_index[i]]
+for i in range(train_count):
+    train_data[i] = data[i]
+    train_labels[i] = labels[i]
+#    train_data[i] = data[shuffled_index[i]]
+#    train_labels[i] = labels[shuffled_index[i]]
 
 #for i in range(test_count):
 #    data_offset = i + train_count    
@@ -124,20 +130,20 @@ print(labels.shape)
 #    val_labels[i] = labels[shuffled_index[data_offset]]
 
 
-#print('Shuffled')
+print('Shuffled')
 
-#with open('preped/train_data.npy', "wb") as f:
-    #numpy.save(f, train_data)
-#with open('preped/train_labels.npy', "wb") as f:
-    #numpy.save(f, train_labels)
+with open('preped/train_data.npy', "wb") as f:
+    numpy.savetxt(f, train_data)
+with open('preped/train_labels.npy', "wb") as f:
+    numpy.savetxt(f, train_labels)
 
 
-#with open('preped/test_data.npy', "wb") as f:
-    #numpy.save(f, test_data)
-#with open('preped/test_labels.npy', "wb") as f:
-    #numpy.save(f, test_labels)
+with open('preped/test_data.npy', "wb") as f:
+    numpy.savetxt(f, test_data)
+with open('preped/test_labels.npy', "wb") as f:
+    numpy.savetxt(f, test_labels)
 
-#with open('preped/val_data.npy', "wb") as f:
-    #numpy.save(f, val_data)
-#with open('preped/val_labels.npy', "wb") as f:
-    #numpy.save(f, val_labels)
+with open('preped/val_data.npy', "wb") as f:
+    numpy.savetxt(f, val_data)
+with open('preped/val_labels.npy', "wb") as f:
+    numpy.savetxt(f, val_labels)
