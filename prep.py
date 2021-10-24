ca770b613e5981247500f1098c5106aaa475010d
Niklas Furberg <niklas.furberg@dynabyte.se>
2021-10-24 19:00:00 +0200

    removed shuffle, added var for setup moves

--- prep.py
+++ prep.py
@@ -7,6 +7,7 @@ start_year = 2020
 end_year = 2020
 dev_mode = True
 output_format = 'TXT'#'TXT' or 'BIN' 
+setup_moves_count = 6
 
 for year in range(start_year, end_year+1):
     root_dir = 'data\gomocup' + str(year) + 'results'
@@ -23,12 +24,6 @@ for year in range(start_year, end_year+1):
                     continue
                 with open(in_directory + filename) as file_in:
                     next(file_in)#Skip initial meta line
-                    next(file_in)#Skip initial setup move
-                    next(file_in)#Skip initial setup move
-                    next(file_in)#Skip initial setup move
-                    next(file_in)#Skip initial setup move
-                    next(file_in)#Skip initial setup move
-                    next(file_in)#Skip initial setup move
                     lines = []
                     for line in file_in:
                         if line.count(',') == 2:
@@ -36,8 +31,8 @@ for year in range(start_year, end_year+1):
 
 print('move count = ' + str(row_count))
 
-data = numpy.zeros(shape=(row_count, 400))
-labels = numpy.zeros(shape=(row_count, 1))
+data = numpy.zeros(shape=(row_count-setup_moves_count, 400))
+labels = numpy.zeros(shape=(row_count-setup_moves_count, 1))
 i = 0
 for year in range(start_year, end_year+1):
     root_dir = 'data\gomocup' + str(year) + 'results'
@@ -54,33 +49,33 @@ for year in range(start_year, end_year+1):
                 if not filename.endswith(".psq"): 
                     continue
                 board = numpy.zeros(shape=(20,20))
-                player = 1
+                current_player = 1
                 eof = False
                 winner = 0
                 j = 0
                 with open(in_directory + filename) as file_in:
-                    next(file_in)
+                    next(file_in)#Skip initial meta line
                     next(file_in)#Skip initial setup move
                     next(file_in)#Skip initial setup move
                     next(file_in)#Skip initial setup move
                     next(file_in)#Skip initial setup move
                     next(file_in)#Skip initial setup move
-                    next(file_in)#Skip initial setup move                    
+                    next(file_in)#Skip initial setup move
                     lines = []
                     for line in file_in:
                         if line.count(',') == 2:
                             data[i] = board.ravel().astype(int)
                             i = i + 1
                             j = j + 1
-                            board[int(line.split(',')[0])-1][int(line.split(',')[1])-1] = player
+                            board[int(line.split(',')[0])-1][int(line.split(',')[1])-1] = current_player
                         elif eof:
                             winner = int(line.split(',')[0])
                         elif line.startswith('-1'):
                             eof = True
-                        if player == 1:
-                            player = 2
+                        if current_player == 1:
+                            current_player = 2
                         else:
-                            player = 1
+                            current_player = 1
                 if winner == 1:
                     labels[i-j:i:2] = 1
                 else:
@@ -93,17 +88,17 @@ print(data.shape)
 print(labels)
 print(labels.shape)
 
-shuffled_index = numpy.zeros(row_count).astype(int)
-for i in range(row_count):
-    shuffled_index[i] = i
+#shuffled_index = numpy.zeros(row_count).astype(int)
+#for i in range(row_count):
+#    shuffled_index[i] = i
 
-numpy.random.shuffle(shuffled_index)
-print(shuffled_index)
+#numpy.random.shuffle(shuffled_index)
+#print(shuffled_index)
 
 
-train_count = int(row_count * 1)
-test_count = int(row_count * 0)
-val_count = int(row_count - train_count - test_count)
+train_count = int((row_count - setup_moves_count) * 1)
+test_count = int((row_count - setup_moves_count) * 0)
+val_count = int((row_count - setup_moves_count) - train_count - test_count)
 print('train_count = ' + str(train_count))
 print('test_count = ' + str(test_count))
 print('val_count = ' + str(val_count))
@@ -116,18 +111,20 @@ val_data = numpy.zeros(shape=(val_count, 400))
 val_labels = numpy.zeros(shape=(val_count, 1))
 
 for i in range(train_count):
-    train_data[i] = data[shuffled_index[i]]
-    train_labels[i] = labels[shuffled_index[i]]
+    # train_data[i] = data[shuffled_index[i]]
+    # train_labels[i] = labels[shuffled_index[i]]
+    train_data[i] = data[i]
+    train_labels[i] = labels[i]
 
 for i in range(test_count):
     j = i + train_count    
-    test_data[i] = data[shuffled_index[j]]
-    test_labels[i] = labels[shuffled_index[j]]
+    test_data[i] = data[j]
+    test_labels[i] = labels[j]
 
 for i in range(val_count):
     j = i + train_count + val_count   
-    val_data[i] = data[shuffled_index[j]]
-    val_labels[i] = labels[shuffled_index[j]]
+    val_data[i] = data[j]
+    val_labels[i] = labels[j]
 
 print('Shuffled')
 
