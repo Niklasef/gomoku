903b5d3c44168ea7eeb9838e728cb5fda855cfd3
Niklas Furberg <niklas.furberg@dynabyte.se>
2022-10-16 21:31:01 +0200

    cnn-64_7-64_5-64_3-64_3-400

--- train.py
+++ train.py
@@ -35,6 +35,7 @@ model = keras.models.Sequential([
     keras.layers.Conv2D(filters=64, kernel_size=(7,7), strides=(1,1), activation='relu', padding="same", input_shape=(20,20,1)),
     keras.layers.Conv2D(filters=64, kernel_size=(5,5), strides=(1,1), activation='relu', padding="same"),
     keras.layers.Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),
+    keras.layers.Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), activation='relu', padding="same"),    
     keras.layers.Flatten(),
     keras.layers.Dense(400, activation='softmax')
 ])
