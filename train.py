26218d07bfbcce0e5cea961c84aab57da9f11b9d
Niklas Furberg <niklas.furberg@dynabyte.se>
2022-10-22 21:14:40 +0200

    Trains on chunks

--- train.py
+++ train.py
@@ -3,32 +3,15 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import subprocess
+import os
 
-# Load data
-# input_format = 'TXT'
-input_format = 'BIN'
-if input_format == 'TXT':
-    train_data = np.genfromtxt('preped/train_data.npy', delimiter=',')
-else:
-    train_data = np.load('preped/train_data.npy')
-if input_format == 'TXT':
-    train_labels = np.genfromtxt('preped/train_labels.npy', delimiter=',')
-else:
-    train_labels = np.load('preped/train_labels.npy')
-if input_format == 'TXT':
-    val_data = np.genfromtxt('preped/val_data.npy', delimiter=',')
-else:
-    val_data = np.load('preped/val_data.npy')
-if input_format == 'TXT':
-    val_labels = np.genfromtxt('preped/val_labels.npy', delimiter=',')
-else:
-    val_labels = np.load('preped/val_labels.npy')
-
-train_data = tf.expand_dims(train_data, axis=-1)
-val_data = tf.expand_dims(val_data, axis=-1)
-
-print(train_data.shape)
-print(train_labels.shape)
+def load_data(file_index):
+    print(f"Loading files with index '{file_index}'")
+    train_data = np.load(f'preped/train_data_{str(file_index)}.npy')
+    train_labels = np.load(f'preped/train_labels_{str(file_index)}.npy')
+    val_data = np.load(f'preped/val_data_{str(file_index)}.npy')
+    val_labels = np.load(f'preped/val_labels_{str(file_index)}.npy')
+    return train_data, train_labels, val_data, val_labels
 
 # Setup Neural Net
 model = keras.models.Sequential([
@@ -46,16 +29,25 @@ model = keras.models.Sequential([
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 
-# Prepare the training dataset
-train_dataset = tf.data.Dataset.from_tensor_slices((train_data, train_labels))
-train_dataset = train_dataset.batch(16)
+file_index_count = 0
+for rootdir, dirs, files in os.walk("preped"):
+    for file in files:
+        if int(file.split('.')[0].split('_')[2]) > file_index_count:
+            file_index_count = int(file.split('.')[0].split('_')[2])
 
-# Prepare the validation dataset
-val_dataset = tf.data.Dataset.from_tensor_slices((val_data, val_labels))
-val_dataset = val_dataset.batch(16)
+for file_index in range(file_index_count):
+    train_data, train_labels, val_data, val_labels = load_data(file_index)
 
-#### Train Model ####
-history = model.fit(train_dataset, epochs=3, validation_data=val_dataset)
+    # Prepare the training dataset
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_data, train_labels))
+    train_dataset = train_dataset.batch(16)
+
+    # Prepare the validation dataset
+    val_dataset = tf.data.Dataset.from_tensor_slices((val_data, val_labels))
+    val_dataset = val_dataset.batch(16)
+
+    #### Train Model ####
+    history = model.fit(train_dataset, epochs=3, validation_data=val_dataset)
 
 label = subprocess.check_output(["git", "describe"]).decode("utf-8").strip()
 print(label)
